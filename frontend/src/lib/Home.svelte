6ce910b4847e331ca4a1751851f94766fecf02dd
Greg Holmes <git@greg.holmes.name>
2025-02-07 19:52:28 +0000

    Remove basically the entire UI

--- frontend/src/lib/Home.svelte
+++ frontend/src/lib/Home.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-  import {EventsOn, LogInfo} from '../../wailsjs/runtime'
-  import {ircmsg} from '../../wailsjs/go/models'
-  let text = ""
-  EventsOn("privmsg", (message: ircmsg.Message) => {
-    LogInfo(message.Source + ": " + message.Params.join(" "))
-  })
 </script>
 
-<p>{text}</p>
+<p>Nothing to see here</p>

a650d2f5e7e5babc75289e0db06ca827e5f8c3c9
Greg Holmes <git@greg.holmes.name>
2025-06-06 21:50:52 +0100

    Add a context menu, with copy/paste/spell checking when needed

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,7 @@ let globalPort
 const parsePort = (win) => {
   return async (data) => {
     const text = new TextDecoder().decode(data)
-    const { port } = /port=(?<port>\d+)/.exec(text).groups
+    const {port} = /port=(?<port>\d+)/.exec(text).groups
     globalPort = port
     win.loadURL(`http://localhost:${globalPort}`)
        .catch(quit)
@@ -26,7 +26,7 @@ const quit = (error) => {
 }
 
 const createWindow = async () => {
-  let port = -1;
+  let port = -1
   const win = new BrowserWindow({
                                   icon:           'icon.png',
                                   width:          800,
@@ -35,6 +35,7 @@ const createWindow = async () => {
                                     autoplayPolicy:       'no-user-gesture-required',
                                     backgroundThrottling: false,
                                     defaultEncoding:      'UTF-8',
+                                    spellcheck:           true,
                                   },
                                 })
   child = spawn(join(__dirname, 'backend'), [], {windowsHide: false})
@@ -46,26 +47,63 @@ const createWindow = async () => {
     return {action: 'deny'}
   })
   const menu = new Menu()
-  menu.append(new MenuItem({
-                             label:       'Refresh',
-                             accelerator: 'F5',
-                             click:       () => {
-                               win.loadURL(`http://localhost:${globalPort}`)
-                                  .catch(e => {
-                                    console.log(e)
-                                    app.quit()
-                                  })
-                             },
-                           }))
-  menu.append(new MenuItem({
-                             label:       'Show Dev Tools',
-                             accelerator: 'F12',
-                             click:       () => {
-                               win.webContents.toggleDevTools()
-                             },
-                           }))
+  menu.append(new MenuItem(
+      {
+        label:       'Refresh',
+        accelerator: 'F5',
+        click:       () => {
+          win.loadURL(`http://localhost:${globalPort}`)
+             .catch(e => {
+               console.log(e)
+               app.quit()
+             })
+        },
+      }))
+  menu.append(new MenuItem(
+      {
+        label:       'Show Dev Tools',
+        accelerator: 'F12',
+        click:       () => {
+          win.webContents.toggleDevTools()
+        },
+      }))
   Menu.setApplicationMenu(menu)
   win.setMenuBarVisibility(false)
+
+  win.webContents.on('context-menu', (event, params) => {
+    if (params.isEditable || params.editFlags.canSelectAll) {
+      const menu = Menu.buildFromTemplate(
+          [
+            {role: 'cut', visible: params.editFlags.canCut},
+            {role: 'copy', visible: params.editFlags.canCopy},
+            {role: 'paste', visible: params.editFlags.canPaste},
+            {role: 'delete', visible: params.editFlags.canDelete},
+            {role: 'selectall', visible: params.editFlags.canCopy},
+          ])
+      if (params.dictionarySuggestions.length > 0 || params.misspelledWord) {
+        menu.append(new MenuItem({type: 'separator'}))
+        for (const suggestion of params.dictionarySuggestions) {
+          menu.append(new MenuItem(
+              {
+                label: suggestion,
+                click: () => win.webContents.replaceMisspelling(suggestion),
+              }))
+        }
+        if (params.misspelledWord) {
+          menu.append(
+              new MenuItem(
+                  {
+                    label: 'Add to dictionary',
+                    click: () => win.webContents.session.addWordToSpellCheckerDictionary(params.misspelledWord),
+                  }),
+          )
+        }
+      }
+      if (menu.items.some((item) => item.visible)) {
+        menu.popup()
+      }
+    }
+  })
 }
 app.setName('tithon')
 app.on('window-all-closed', () => {
